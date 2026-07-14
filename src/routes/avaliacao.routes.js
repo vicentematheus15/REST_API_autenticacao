35c386c849cffc98f0dae21c0a11e009ef40e5ba
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-14 20:40:31 -0300

    refactor: remove unused evaluation routes and models

--- src/routes/avaliacao.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as controller from '../controllers/avaliacao.controller.js';
-import {autenticar} from '../middlewares/auth.middleware.js';
-
-const avaliacaoRoutes = Router();
-
-avaliacaoRoutes.post('/diagnostica', autenticar, controller.gerar);
-
-export default avaliacaoRoutes;
